6001ec0faee0dc4ab411d0b000fa40c5ec835671
Maarten Berkenbosch <maarten@beebase.nl>
2015-06-19 21:42:40 +0200

    p117: create user schema model

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,6 +2,8 @@ var config = require('./config');
 var mongoose = require('mongoose');
 
 module.exports = function() {
-  var db = mongoose.connnect(config.db);
+  var db = mongoose.connect(config.db);
+  require('../app/models/user.server.model');
   return db;
 };
+
