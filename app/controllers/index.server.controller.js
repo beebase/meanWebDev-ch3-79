63aaff68011cbf5e2f356158b3fe89d13a2e12bd
Maarten Berkenbosch <maarten@beebase.nl>
2015-06-19 20:51:56 +0200

    add new line in log

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,7 +1,7 @@
 exports.render = function(req, res) {
   // add lastVisit to session object
   if (req.session.lastVisit) {
-    console.log('LAST VISIT: ' + req.session.lastVisit);
+    console.log('\nLAST VISIT: ' + req.session.lastVisit);
   }
   req.session.lastVisit = new Date();
 
