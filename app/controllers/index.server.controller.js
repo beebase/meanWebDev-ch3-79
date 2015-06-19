2bce55b4adb97046fe9412a4e5ed6dd654468545
Maarten Berkenbosch <maarten@beebase.nl>
2015-06-19 20:28:57 +0200

    p89: typo

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,7 +1,5 @@
 exports.render = function(req, res) {
-  exports.render = function(req, res) {
-    res.render('index', {
-      title: 'Hello World'
-    })
-  };
+  res.render('index', {
+    title: 'Hello World'
+  })
 };
