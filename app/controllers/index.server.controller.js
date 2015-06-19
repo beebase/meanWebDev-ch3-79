0ced63dca8f95e6306b4d8b1aeed094ac9fc0405
Maarten Berkenbosch <maarten@beebase.nl>
2015-06-19 20:45:16 +0200

    p94: add lastVisit to session object

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,4 +1,10 @@
 exports.render = function(req, res) {
+  // add lastVisit to session object
+  if (req.session.lastVisit) {
+    console.log('LAST VISIT: ' + req.session.lastVisit);
+  }
+  req.session.lastVisit = new Date();
+
   res.render('index', {
     title: 'Hello World'
   })
