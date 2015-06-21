370cfc677fe5db26f8767fcc9c7c183c40290a05
Maarten Berkenbosch <maarten@beebase.nl>
2015-06-21 22:34:27 +0200

    Add users modules + auth service

--- public/application.js
+++ public/application.js
@@ -1,12 +1,15 @@
 var mainApplicationModuleName = 'mean';
 var mainApplicationModule = angular.module(mainApplicationModuleName, [
-  'ngRoute',
-  'example'
+  'ngRoute', 'users', 'example'
 ]);
 
 mainApplicationModule.config(function($locationProvider) {
   $locationProvider.hashPrefix('!');
 });
+
+// hack fix for facebook bug
+if (window.location.hash === '#_=_') window.location.hash = '#!';
+
 
 angular.element(document).ready(function() {
   angular.bootstrap(document, [mainApplicationModuleName]);
