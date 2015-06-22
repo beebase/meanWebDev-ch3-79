63a043a8f338680a3cfe3ce605fa09ccacd3eaf2
Maarten Berkenbosch <maarten@beebase.nl>
2015-06-22 10:30:18 +0200

    p230: Add create article

--- public/articles/controllers/articles.client.controller.js
+++ public/articles/controllers/articles.client.controller.js
@@ -1,4 +1,19 @@
 angular.module('articles').controller('ArticlesController',
   function($scope, $routeParams, $location, Authentication, Articles) {
     $scope.authentication = Authentication;
+
+    $scope.create = function() {
+      var article = new Articles({
+        title  : this.title,
+        content: this.content
+      });
+
+      article.$save(
+        function(response) {
+          $location.path('articles/' + response._id);
+        },
+        function(errorResponse) {
+          $scope.error = errorResponse.data.message;
+        });
+    }
   });
